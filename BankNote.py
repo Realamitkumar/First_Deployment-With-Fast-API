be34dcb733ac80252a175d2da114905cf73fb30f
Amit kumar <96509000+Realamitkumar@users.noreply.github.com>
2022-02-19 04:50:10 +0530

    Add files via upload

---
+++ BankNote.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 18 18:57:09 2022
+
+@author: amitt
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 17 21:51:19 2020
+
+@author: win10
+"""
+from pydantic import BaseModel
+# 2. Class which describes Bank Notes measurements
+class BankNote(BaseModel):
+    variance: float 
+    skewness: float 
+    curtosis: float 
+    entropy: float
